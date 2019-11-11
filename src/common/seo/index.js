3012788b21ead661e1e7f2ef64761d4a3f734d6f
anapal7 <55529484+anapal7@users.noreply.github.com>
2019-11-12 00:09:03 +1100

    Update index.js

--- src/common/seo/index.js
+++ src/common/seo/index.js
@@ -18,6 +18,7 @@ export default props => (
       <Helmet>
         <title>{props.title.replace(/\b\w/g, letter => letter.toUpperCase()) + " - " + data.site.siteMetadata.title}</title>
         <meta name="description" content={props.description} />
+         <link rel="canonical" href="${data.site.siteMetadata.siteUrl}${props.slug}" />
       </Helmet>
     )} />
 )
