388f4a8906bca82404335c8f2d97e3c9ba2dde86
anapal7 <55529484+anapal7@users.noreply.github.com>
2020-02-21 00:03:40 +1100

    Update about.js

--- src/homepage/components/about.js
+++ src/homepage/components/about.js
@@ -15,10 +15,10 @@ export default () => (
       }  
     `}
     render={(data) => (
-      <div className="bg-dark-blue flex flex-column justify-center items-center pa2 pv5">
+      <div className="bg-light-blue flex flex-column justify-center items-center pa2 pv5">
         <h1 className="fw1 display db white f2 tc">{data.site.siteMetadata.homepageHeader}</h1>
-        <p className="f4 serif mw7 tc lh-copy">{data.site.siteMetadata.homepageAbout}</p>
-        <Link to="/about/" className="mt3 db no-underline ph5 pv3 sans-serif dark-grey bg-white ttu tracked b hover-bg-mid-gray">About Me</Link>
+        <p className="f4 serif white mw7 tc lh-copy">{data.site.siteMetadata.homepageAbout}</p>
+        <Link to="/about/" className="mt3 db no-underline ph5 pv3 sans-serif dark-grey bg-white ttu tracked b hover-bg-near-white">About Me</Link>
       </div>
     )} />
 )
