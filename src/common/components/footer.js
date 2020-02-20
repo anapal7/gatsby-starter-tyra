734e54503fd1a9e20d8714ebd309886f7d4804bd
anapal7 <55529484+anapal7@users.noreply.github.com>
2020-02-20 20:32:20 +1100

    Update footer.js

--- src/common/components/footer.js
+++ src/common/components/footer.js
@@ -99,7 +99,7 @@ export default () => (
           </div>
         </div>
         <div className="w-100 mw9 silver center sans-serif f6">
-          <p>Tyra Theme (C) 2018 by Madelyn Eriksen under terms of the "MIT" software license.</p>
+          <p>Gatsby Theme (C) 2019.</p>
         </div>
       </footer>
     )} />
